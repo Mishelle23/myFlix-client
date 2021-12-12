93cf64c3f84187223569a19808d7bea5b611d33a
Mishelle23 <michaela.med@outlook.com>
2021-12-12 18:42:54 +0000

    task 3.4

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -11,12 +12,13 @@ export class MainView extends React.Component {
     super();
     this.state = {
       movies: [],
-      selectedMovie: null
+      selectedMovie: null,
+      user: null
     };
   }
 
   componentDidMount() {
-    axios.get('https://[app name].herokuapp.com/movies')
+    axios.get('https://safe-coast-49930.herokuapp.com/movies')
       .then(response => {
         this.setState({
           movies: response.data
@@ -27,14 +29,22 @@ export class MainView extends React.Component {
       });
   }
 
-  setSelectedMovie(newSelectedMovie) {
+  setSelectedMovie(movie) {
     this.setState({
-      selectedMovie: newSelectedMovie
+      selectedMovie: movie
+    });
+  }
+
+  onLoggedIn(user) {
+    this.setState({
+      user
     });
   }
 
   render() {
     const { movies, selectedMovie } = this.state;
+
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
     if (movies.length === 0) return <div className="main-view" />;
 
@@ -43,7 +53,7 @@ export class MainView extends React.Component {
         {selectedMovie ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => {
           this.setSelectedMovie(newSelectedMovie);
         }} /> : movies.map(movie => (<MovieCard key={movie._id} movie={movie}
-          onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+          onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
         ))
         }
       </div>
